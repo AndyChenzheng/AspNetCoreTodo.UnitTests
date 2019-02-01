d18597980708240d5a14d20d3d7008f766d0e1b2
andy.chen <405643529@qq.com>
2019-02-01 21:07:01 +0800

    测试项目

---
+++ TodoItemServiceShould.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Threading.Tasks;
+using AspNetCoreTodo.Data;
+using AspNetCoreTodo.Models;
+using AspNetCoreTodo.Services;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Xunit;
+
+namespace AspNetCoreTodo.UnitTests
+{
+    public class TodoItemServiceShould
+    {
+        [Fact]
+        public async Task AddNewItemAsIncompleteWithDueDate()
+        {
+            //要引用Microsoft.EntityFrameworkCore.InMemory
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseInMemoryDatabase(databaseName: "Test_AddNewItem").Options;
+
+            using (var context=new ApplicationDbContext(options))
+            {
+                var service = new TodoItemService(context);
+
+                var fackUser = new IdentityUser
+                {
+                    Id = "fack-000",
+                    UserName = "fack@example.com"
+                };
+
+                await service.AddItemAsync(new TodoItem
+                {
+                    Title = "Testing?"
+
+                }, fackUser);
+
+            }
+
+            using (var context=new ApplicationDbContext(options))
+            {
+                var itemsInDatabase = await context.Items.CountAsync();
+                Assert.Equal(1,itemsInDatabase);
+
+                var item = await context.Items.FirstAsync();
+                Assert.Equal("Testing?",item.Title);
+                Assert.False(item.IsDone);
+
+                var difference = DateTimeOffset.Now.AddDays(3) - item.DueAt;
+                Assert.True(difference<TimeSpan.FromSeconds(1));
+            }
+
+
+        }
+    }
+}
